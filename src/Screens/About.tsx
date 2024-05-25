4fdeacfc55d89c117d2fd1665d626befea7128b8
vidhiwalia <vidhiwalia16@gmail.com>
2024-05-25 20:29:10 +0530

    Vidhi

---
+++ src/Screens/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <div>Hello i am about section
+        <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,30 @@
 import React from "react";
-import Header from "../components/Header/Header";
+import Link from 'next/link';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Header from "../components/Header/Header";
+import ACarousel from "../components/ACarousel/ACarousel";
+import Cards from "../components/Cards/Cards";
+
+//import About from '../Screens/About';
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  
-  
-  return ( 
+
+  return (
     <>
-    <div className="bg-black h-[20%]">
-    <Header />
-    </div>
-    
+      <div className="bg-black h-[20%]">
+        <Header />
+      </div>
+      <div>
+        <ACarousel/>
+        </div>
+        <div>
+        <Cards/>
+        </div>
+        
+      
+        
     </>
   );
 }
